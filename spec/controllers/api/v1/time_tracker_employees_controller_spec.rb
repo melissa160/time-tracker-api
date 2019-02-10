eb6bfe5646d42e3662d6d5e5b66312ff2ea4858e
melissa <melissa160@gmail.com>
2019-02-09 22:54:56 -0500

    adding test to time-tracker-employees/out endpoint

--- spec/controllers/api/v1/time_tracker_employees_controller_spec.rb
+++ spec/controllers/api/v1/time_tracker_employees_controller_spec.rb
@@ -25,6 +25,28 @@ RSpec.describe 'time tracker employees controller', type: :request do
         end
       end
     end
+
+    describe 'Create time-out register' do
+      let(:tag) { Faker::HarryPotter.location }
+      let(:valid_attributes) { { time_tracker_employee: { tag: tag, user_id: employee.id } } }
+
+      context 'Create time-out register succesfully' do
+        before { post "/api/v1/time-tracker-employees/out", params: valid_attributes }
+
+        it 'returns status code 201' do
+          expect(response).to have_http_status(201)
+        end
+      end
+
+      context 'Not posible create register, employee id does not exist' do
+        let(:invalid_attributes) { { time_tracker_employee: { tag: tag, user_id: employee.id+1 } }}
+        before { post "/api/v1/time-tracker-employees/out", params: invalid_attributes }
+
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
+      end
+    end
   end
 
   context 'with employee user' do
@@ -47,6 +69,28 @@ RSpec.describe 'time tracker employees controller', type: :request do
       context 'Not posible create register, employee id does not exist' do
         let(:invalid_attributes) { { time_tracker_employee: { tag: tag, user_id: employee.id+1 } }}
         before { post "/api/v1/time-tracker-employees/in", params: invalid_attributes }
+
+        it 'returns status code 403' do
+          expect(response).to have_http_status(403)
+        end
+      end
+    end
+
+    describe 'Create time-out register' do
+      let(:tag) { Faker::HarryPotter.location }
+      let(:valid_attributes) { { time_tracker_employee: { tag: tag, user_id: employee.id } } }
+
+      context 'Create time-out register succesfully' do
+        before { post "/api/v1/time-tracker-employees/out", params: valid_attributes }
+
+        it 'returns status code 403' do
+          expect(response).to have_http_status(403)
+        end
+      end
+
+      context 'Not posible create register, employee id does not exist' do
+        let(:invalid_attributes) { { time_tracker_employee: { tag: tag, user_id: employee.id+1 } }}
+        before { post "/api/v1/time-tracker-employees/out", params: invalid_attributes }
 
         it 'returns status code 403' do
           expect(response).to have_http_status(403)
