7f913f9af028f7c6409ed5e7190668200271a85d
melissa <melissa160@gmail.com>
2019-02-09 19:35:09 -0500

    user controller spec completed

--- spec/controllers/api/v1/user_controller_spec.rb
+++ spec/controllers/api/v1/user_controller_spec.rb
@@ -51,4 +51,54 @@ RSpec.describe 'users API', type: :request do
       end
     end
   end
+
+  context 'with employee user' do
+    let(:user) { create(:user) }
+    before { allow_any_instance_of(DecodeAuthCommand).to receive(:result).and_return(user) }
+    describe 'API User' do
+      let(:email) { Faker::Internet.email }
+      let(:password) { Faker::Internet.password }
+      let(:valid_attributes) { { email: email, password: password } }
+
+      context 'Create user, role employee' do
+        before { post "/api/v1/users", params: valid_attributes }
+
+        it 'returns status code 402' do
+          expect(response).to have_http_status(403)
+        end
+      end
+
+      context 'Show user by id' do
+        before { get "/api/v1/users/#{user_id}"}
+
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+      end
+
+      context 'List all users' do
+        before { get '/api/v1/users'}
+
+        it 'returns status code 402' do
+          expect(response).to have_http_status(403)
+        end
+      end
+
+      context 'Update users' do
+        before { put "/api/v1/users/#{user_id}", params: valid_attributes }
+
+        it 'returns status code 402' do
+          expect(response).to have_http_status(403)
+        end
+      end
+
+      context 'delete users' do
+        before { delete "/api/v1/users/#{user_id}" }
+
+        it 'returns status code 402' do
+          expect(response).to have_http_status(403)
+        end
+      end
+    end
+  end
 end
